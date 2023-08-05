c321ec46d29a6b6d5d2015a78a57b52ce83a9fcb
Albinson Arias <103264552+BITS-albinsonarias@users.noreply.github.com>
2023-08-05 10:57:35 -0400

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.simple_expense_tracker_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:simple_expense_tracker_bloc/app/app.dart';
+import 'package:simple_expense_tracker_bloc/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
